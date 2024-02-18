d8c3f60a2be62e3c9945822dfc014095a090b1ac
PivoEnjoyer62 <130290987+ShabaIMMERSION@users.noreply.github.com>
2024-02-18 11:52:51 +0200

    sigmasvinota

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template("main.html")
+
+
+@app.route('/quizzes')
+def quizzes():
     sql = SQLManager("quizz.db")
     quizzes = sql.select_quizzes()
-    return render_template("index.html", quizzes=quizzes)
+    return render_template("quizzes.html", quizzes=quizzes)
+
 
 
 if __name__ == "__main__":
